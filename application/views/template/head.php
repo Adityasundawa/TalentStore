4ac38842af3e0136246946c74f0787e228f002dd
Adityasundawa <adityasundawa.co@gmail.com>
2020-05-25 22:31:43 +0700

    Add Liblary Mobile & Detskop

---
+++ application/views/template/head.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="<?= base_url('assets/');?>images/favicon.png" rel="shortcut icon">
+    <title>Talent Store | <?php echo $title;?></title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800" rel="stylesheet">
+    <link rel="stylesheet" href="<?= base_url('assets/');?>css/vendor.css">
+    <link rel="stylesheet" href="<?= base_url('assets/');?>css/utility.css">
+    <link rel="stylesheet" href="<?= base_url('assets/');?>css/app.css">
+</head>
